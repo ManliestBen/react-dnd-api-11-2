d6ff16958aeed97de9bcd19a83ca67288088f030
ManliestBen <benjamintmanley@gmail.com>
2021-01-06 09:03:58 -0600

    map classes to divs

--- src/pages/ClassList/ClassList.jsx
+++ src/pages/ClassList/ClassList.jsx
@@ -2,13 +2,26 @@ import React, { Component } from 'react'
 
 class ClassList extends Component {
   state = { 
-    results: ['Paladin', 'Bard', 'Rogue']
+    results: ['Paladin', 'Bard', 'Rogue', 'Wizard']
    }
-   
+
   render() { 
     return ( 
       <div>
-        <h3>I'm the Class List</h3>
+        <h3>Class List</h3>
+        <div className="icon-container">
+          {/* This is where we map over our results, representing each with a div containing an image and a name */}
+          {this.state.results.map((classTitle) => (
+            <div id="classDiv" key={classTitle}>
+              <img 
+								style={{ width: "100px", height: "100px" }}
+								src={`/images/${classTitle}.svg`} 
+								alt="class-logo"
+							/>
+              {classTitle}
+            </div>
+          ))}
+        </div>
       </div>
      );
   }
